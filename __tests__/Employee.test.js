14028ffc85033c79b7d43b1cf28333ac2dcf4a5a
patrickarcher414 <patrickarcher414@gmail.com>
2022-05-07 13:34:17 -0500

    employee tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,29 @@
+// Import data from Employee.js
+const Employee = require("../lib/Employee");
+// Instantiate new Employee Object
+const Employee = new Employee();
+
+// test employee constructor
+test("creates a new Employee object", (req, res) => {
+  expect(res).toBe();
+});
+
+// test getName function
+test("returns the name of employee", (req, res) => {
+  expect(res).toBe();
+});
+
+// test getId function
+test("returns the id of employee", (req, res) => {
+  expect(res).toBe();
+});
+
+// test getEmail
+test("returns the email of employee", (req, res) => {
+  expect(res).toBe();
+});
+
+// test getRole
+test("returns the role of employee", (req, res) => {
+  expect(res).toBe();
+});
---
+++ src/template.js
@@ -0,0 +1,7 @@
+const genHTML = (data) => {
+  return `
+<h1>Team Profile<h1>
+
+${data.name}
+`;
+};
