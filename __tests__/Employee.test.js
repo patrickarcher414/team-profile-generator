ea0a92c7cf36aac87e2db1e159a07d91e023b69e
patrickarcher414 <patrickarcher414@gmail.com>
2022-05-08 20:42:03 -0500

    employee tests pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,32 +1,41 @@
 // Import data from Employee.js
 const Employee = require("../lib/Employee");
+
+// // Follow this method of writing tests
+// Arrange
+// Act
+// Assert
+
 // Instantiate new Employee Object
 test("should instantiate a new employee", () => {
   let employee = new Employee();
   expect(typeof employee).toBe("object");
 });
 
-// test employee constructor
-test("creates a new Employee object", (req, res) => {
-  expect(res).toBe();
-});
-
 // test getName function
-test("returns the name of employee", (req, res) => {
-  expect(res).toBe();
+test("returns the name of employee", () => {
+  const employee = new Employee("Dave", 1, "dave@email.com");
+  const name = employee.getName();
+  expect(name).toBe("Dave");
 });
 
 // test getId function
-test("returns the id of employee", (req, res) => {
-  expect(res).toBe();
+test("returns the id of employee", () => {
+  const employee = new Employee("Dave", 1, "dave@email.com");
+  const id = employee.getId();
+  expect(id).toBe(1);
 });
 
 // test getEmail
-test("returns the email of employee", (req, res) => {
-  expect(res).toBe();
+test("returns the email of employee", () => {
+  const employee = new Employee("Dave", 1, "dave@email.com");
+  const email = employee.getEmail();
+  expect(email).toBe("dave@email.com");
 });
 
 // test getRole
-test("returns the role of employee", (req, res) => {
-  expect(res).toBe();
+test("returns the role of employee", () => {
+  const employee = new Employee("Dave", 1, "dave@email.com");
+  const role = employee.getRole();
+  expect(role).toBe("Employee");
 });
